3d99403480ef5abefbf5f53dec714c9a0c7d184e
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-17 18:49:40 +0530

    project-15

--- project-15/script.js
+++ project-15/script.js
@@ -1,7 +1,28 @@
+let features = [
+    {
+        title:'Bookmark in one click',
+        content: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
+        image: './images/illustration-features-tab-1.svg'
+    },
+    {
+        title:'Intelligent search',
+        content: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
+        image:'./images/illustration-features-tab-2.svg'
+    },
+    {
+        title:'Share your bookmarks',
+        content: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
+        image: './images/illustration-features-tab-3.svg'
+    }
+]
 let ham = document.getElementById('ham')
 let tabs = document.getElementById('tabs')
 let Close = document.getElementById('icon-close')
-
+let FeatureTab = document.querySelector('#features-tab ul')
+let FeaturesTitle = document.getElementById('features-info-title')
+let FeaturesContent = document.getElementById('features-info-content')
+let FeaturesImage = document.querySelector('#features-info-image img')
+console.log(FeaturesImage.src)
 ham.addEventListener('click',()=>{
     tabs.classList.remove('left-full')
     tabs.classList.add('left-0')
@@ -10,4 +31,10 @@ Close.addEventListener('click',()=>{
     tabs.classList.remove('left-0')
     tabs.classList.add('left-full')
     console.log(tabs.classList)
+})
+
+FeatureTab.addEventListener('click',(e)=>{
+    FeaturesTitle.innerText = features[e.target.id-1]['title']
+    FeaturesContent.innerText = features[e.target.id-1]['content']
+    FeaturesImage.src = features[e.target.id-1]['image'];
 })
