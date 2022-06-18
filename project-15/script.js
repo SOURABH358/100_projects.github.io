2dd40aa801c2e422f831e5c3ccfb902e12ee619f
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-18 09:51:55 +0530

    project-15

--- project-15/script.js
+++ project-15/script.js
@@ -1,16 +1,16 @@
 let features = [
     {
-        title:'Bookmark in one click',
+        title: 'Bookmark in one click',
         content: 'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.',
         image: './images/illustration-features-tab-1.svg'
     },
     {
-        title:'Intelligent search',
+        title: 'Intelligent search',
         content: 'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.',
-        image:'./images/illustration-features-tab-2.svg'
+        image: './images/illustration-features-tab-2.svg'
     },
     {
-        title:'Share your bookmarks',
+        title: 'Share your bookmarks',
         content: 'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.',
         image: './images/illustration-features-tab-3.svg'
     }
@@ -24,23 +24,28 @@ let FeaturesContent = document.getElementById('features-info-content')
 let FeaturesImage = document.querySelector('#features-info-image img')
 let Faq = document.querySelectorAll('#faqs li')
 
-ham.addEventListener('click',()=>{
+ham.addEventListener('click', () => {
     tabs.classList.remove('left-full')
     tabs.classList.add('left-0')
 })
-Close.addEventListener('click',()=>{
+Close.addEventListener('click', () => {
     tabs.classList.remove('left-0')
     tabs.classList.add('left-full')
     console.log(tabs.classList)
 })
 
-FeatureTab.addEventListener('click',(e)=>{
-    FeaturesTitle.innerText = features[e.target.id-1]['title']
-    FeaturesContent.innerText = features[e.target.id-1]['content']
-    FeaturesImage.src = features[e.target.id-1]['image'];
+FeatureTab.addEventListener('click', (e) => {
+    FeaturesTitle.innerText = features[e.target.id - 1]['title']
+    FeaturesContent.innerText = features[e.target.id - 1]['content']
+    FeaturesImage.src = features[e.target.id - 1]['image'];
 })
 Faq.forEach(element => {
-    element.addEventListener('click',(e)=>{
+    element.addEventListener('click', (e) => {
+        Faq.forEach((item)=>{
+            item.querySelector('.answer').classList.add('hidden')
+            item.querySelector('.arrow-up').classList.add('hidden')
+            item.querySelector('.arrow-down').classList.remove('hidden')
+        })
         e.currentTarget.querySelector('.answer').classList.toggle('hidden')
         e.currentTarget.querySelector('.arrow-up').classList.toggle('hidden')
         e.currentTarget.querySelector('.arrow-down').classList.toggle('hidden')
