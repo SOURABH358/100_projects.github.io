9fe169aed33946c8b6e21e6b739f77d016e9790b
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-22 21:51:25 +0530

    project-16

--- project-16/tailwind.config.js
+++ project-16/tailwind.config.js
@@ -2,27 +2,27 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {
-      fontFamily:{
-        'Rubik': ['Rubik']
-      }
-    },
-    colors: {
-      'Blue': 'hsl(246, 80%, 60%)',
-      'Light-red': 'hsl(15, 100 %, 70 %)',
-      'Soft-blue': 'hsl(195, 74 %, 62 %)',
-      'Light-red': 'hsl(348, 100 %, 68 %)',
-      'Lime-green': 'hsl(145, 58 %, 55 %)',
-      'Violet': 'hsl(264, 64 %, 52 %)',
-      'Soft-orange': 'hsl(43, 84 %, 65 %)',
-      'Very-dark-blue': 'hsl(226, 43 %, 10 %)',
-      'Dark-blue': 'hsl(235, 46 %, 20 %)',
-      'Desaturated-blue': 'hsl(235, 45 %, 61 %)',
-      'Pale-Blue': 'hsl(236, 100 %, 87 %)'
-    },
-    fontSize:{
-      base:['18px']
-    }
+        fontFamily:{
+          'Rubik': ['Rubik','sans-serif']
+        },
+        fontSize:{
+          base:'18px'
+        },
+      colors: {
+        'Blue': 'hsl(246, 80%, 60%)',
+        'Light-red': 'hsl(15, 100%, 70%)',
+        'Soft-blue': 'hsl(195, 74%, 62%)',
+        'Light-red': 'hsl(348, 100%, 68%)',
+        'Lime-green': 'hsl(145, 58%, 55%)',
+        'Violet': 'hsl(264, 64%, 52%)',
+        'Soft-orange': 'hsl(43, 84%, 65%)',
+        'Verydarkblue': 'hsl(226, 43%, 10%)',
+        'Dark-blue': 'hsl(235, 46%, 20%)',
+        'Desaturated-blue': 'hsl(235, 45%, 61%)',
+        'Pale-Blue': 'hsl(236, 100%, 87%)',
+        'white':'#fff'
+      },
+   
   },
   plugins: [],
 }
