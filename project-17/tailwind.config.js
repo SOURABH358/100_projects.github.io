5cfabf1746579f9b6d3627e1fc12241a26c893b3
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-06-27 20:37:20 +0530

    project-17

--- project-17/tailwind.config.js
+++ project-17/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Dark-cyan': 'hsl(158, 36%, 37%)',
+        'Cream': 'hsl(30, 38%, 92%)',
+        'Very-dark-blue': 'hsl(212, 21%, 14%)',
+        'Dark-grayish-blue': 'hsl(228, 12%, 48%)',
+        'White': 'hsl(0, 0%, 100%)'
+      },
+      fontSize: {
+        'base': '14px'
+      }
+      , fontFamily: {
+        'Fraunces': ['Fraunces', 'serif'],
+        'Montserrat': ['Montserrat', 'sans- serif'],
+      },
+      backgroundImage: {
+        'hero-mobile': "url('./images/image-product-mobile.jpg')",
+        'hero-desktop': "url('./images/image-product-desktop.jpg')"
+      }
+    }
+
   },
   plugins: [],
 }
