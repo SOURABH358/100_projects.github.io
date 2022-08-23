004935632e7d704841f8eb77b8e136ca54ecd27c
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-08-23 21:37:21 +0530

    added changes

--- project-18/script.js
+++ project-18/script.js
@@ -1,6 +1,17 @@
-let str = 'HelloWorld';
-const resQr = document.querySelector('#result__qr img')
-function getQR(){
-    resQr.src = `http://api.qrserver.com/v1/create-qr-code/?data=${str}!&size=100x100`
-}
-getQR();
+const resQr = document.querySelector('.result__qr img')
+const generateButton = document.querySelector('#generate__btn');
+const inputText = document.querySelector('#generator__form input')
+generateButton.addEventListener('click',()=>{
+    let str = inputText.value;
+    str=str.split(" ").join("");
+    console.log(str)
+    if(str){
+    
+        document.querySelector('.result__qr').classList.add('show__result__qr');
+        resQr.setAttribute('src', `http://api.qrserver.com/v1/create-qr-code/?data=${str}!&size=100x100`);
+        console.log(resQr.src)
+    }
+    else{
+        window.alert('Please provide any text or url')
+    }
+});
