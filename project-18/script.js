b53d150aaea450db00821ceb1fd4759cebb7f82c
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-08-23 21:56:06 +0530

    added changes

--- project-18/script.js
+++ project-18/script.js
@@ -1,15 +1,15 @@
 const resQr = document.querySelector('.result__qr img')
 const generateButton = document.querySelector('#generate__btn');
 const inputText = document.querySelector('#generator__form input')
-generateButton.addEventListener('click',()=>{
+const resCont = document.querySelector('.result__qr');
+generateButton.addEventListener('click',(e)=>{
+    e.preventDefault();
     let str = inputText.value;
     str=str.split(" ").join("");
-    console.log(str)
     if(str){
-    
-        document.querySelector('.result__qr').classList.add('show__result__qr');
-        resQr.setAttribute('src', `http://api.qrserver.com/v1/create-qr-code/?data=${str}!&size=100x100`);
-        console.log(resQr.src)
+        
+        resQr.setAttribute('src', `http://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${str}`);
+        resCont.classList.add('show__result__qr');
     }
     else{
         window.alert('Please provide any text or url')
