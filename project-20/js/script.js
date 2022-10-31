b3b334d098c4c947ab3c73919f4c0e1976e3d5f1
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-10-31 19:11:06 +0530

    added files

--- project-20/js/script.js
+++ project-20/js/script.js
@@ -3,6 +3,8 @@ const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
 const start = document.getElementById('start')
 const result = document.getElementById('result')
 const Alert = document.querySelector('.alert')
+const Copy = document.getElementById('copy')
+
 start.addEventListener('click',()=>{
     recognition.start();
     console.log('speech recognition has started')
@@ -24,3 +26,16 @@ recognition.onend = (event)=>{
     recognition.onresult = (event)=>{
         result.innerText = event.results[0][0].transcript;
     }
+
+    Copy.addEventListener('click',()=>{
+        if(result.innerText === 'Click the button to start recording')
+            navigator.clipboard.writeText('')
+        else
+            navigator.clipboard.writeText(result.innerText)
+
+            Alert.classList.add('show__alert')
+            Alert.innerText = 'Copied to Clipboard!'
+            setTimeout(()=>{
+                Alert.classList.remove('show__alert')
+            },1500)
+    })
