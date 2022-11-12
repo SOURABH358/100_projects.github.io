a517025a6cd5a2870f87ae5054969e8392638b97
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-11-12 19:12:03 +0530

    added files

--- project-25/user-sign-in/src/Typography.js
+++ project-25/user-sign-in/src/Typography.js
@@ -1,16 +1,108 @@
 import React from "react";
-import { Grid, Typography } from '@mui/material'
+import { Grid, Paper, Typography, Box, TextField, Checkbox, FormControlLabel, Button, Link } from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Avatar from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Avatar } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme()
 function TypographyExample() {
-    const theme = createTheme()
-    return
-    <ThemeProvider theme = {theme}>
-        <Grid container>
-            <Avatar sx={{ bgcolor: secondary}}>
-                <LockOutlinedIcon />
-            </Avatar>
+    return <ThemeProvider theme={theme}>
+        <Grid container component='main'
+            sx={{
+                minHeight: '100vh',
+            }}>
+            <CssBaseline />
+            <Grid item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    background: 'url(https://source.unsplash.com/random)',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: 'center'
+                }}>
+
+            </Grid>
+            <Grid item component={Paper} xs={12} sm={8} md={5}>
+                <Box
+                    sx={{
+                        mt: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center'
+                    }}>
+                    <Avatar sx={{ bgcolor: 'secondary.main', m: 1 }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component={"h1"} variant={"h5"}>Sign in</Typography>
+                </Box>
+                <Box item component='form' sx={{ mt: 1, mx: 4, }}>
+                    <TextField
+                        required
+                        fullWidth
+                        margin="normal"
+                        autoComplete="email"
+                        autoFocus
+                        id="email"
+                        label="Email"
+                        variant="outlined"
+                        sx={{
+
+                        }}>
+
+                    </TextField>
+                    <TextField
+                        required
+                        fullWidth
+                        margin="normal"
+                        autoComplete="current-password"
+                        autoFocus
+                        id="password"
+                        label="Password"
+                        variant="outlined"
+                        sx={{
+
+                        }}>
+
+                    </TextField>
+                    <FormControlLabel control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me" />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{
+                            mt: 4,
+                            mb: 2
+                        }}
+                    >
+                        Sign in
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                            {"Forgot Password?"}
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                            {"Don't have an account? Sign up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                    <Box align="center"sx={{mt:4}}>
+                        <Typography variant="body2" color='text.secondary'>{'Copyright © '}
+                        <Link color= "inherit"href = "#">Your Website</Link>{'  '}
+                        {new Date().getFullYear()}{'.'}
+                        </Typography>
+                    </Box>
+                </Box>
+
+
+            </Grid>
         </Grid>
     </ThemeProvider>
 }
